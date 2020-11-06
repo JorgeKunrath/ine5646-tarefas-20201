576ed3e5c26781301a37804ba18eb30c31ee73e7
Jorge Kunrath <jorgeks95@gmail.com>
2020-11-05 21:53:20 -0300

    atualizando a desgraça da porta de novo

--- 03-app_filmes/servidor/src/app.js
+++ 03-app_filmes/servidor/src/app.js
@@ -9,7 +9,7 @@ const opcoes = {
   cert: fs.readFileSync(path.resolve(__dirname, '../cert/cert.pem'))
 }
 
-const PORTA = 3002
+const PORTA = 3003
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, '../publico')))
